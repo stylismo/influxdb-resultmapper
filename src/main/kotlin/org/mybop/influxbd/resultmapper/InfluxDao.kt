6976229162984fcaa78b65716dee54467b54132e
Gautier Levert <gautier.levert@gmail.com>
2018-06-08 14:31:41 +0200

    Add custom query result

--- src/main/kotlin/org/mybop/influxbd/resultmapper/InfluxDao.kt
+++ src/main/kotlin/org/mybop/influxbd/resultmapper/InfluxDao.kt
@@ -9,7 +9,7 @@ import kotlin.reflect.KClass
 
 class InfluxDao<K : Any>(
         clazz: KClass<K>,
-        registry: ConverterRegistry,
+        private val registry: ConverterRegistry,
         private val client: InfluxDB
 ) {
 
@@ -44,12 +44,31 @@ class InfluxDao<K : Any>(
 
     fun queryOne(query: String) = queryList(query).first()
 
+    fun <T : Any> queryOne(query: String, clazz: KClass<T>) = queryList(query, clazz).first()
+
+    fun <T : Any> queryOne(query: String, clazz: Class<T>) = queryOne(query, clazz.kotlin)
+
     fun queryList(query: String) = query(query).first().flatMap { it.value }
 
+    fun <T : Any> queryList(query: String, clazz: KClass<T>) = query(query, clazz).first().flatMap { it.value }
+
+    fun <T : Any> queryList(query: String, clazz: Class<T>) = queryList(query, clazz.kotlin)
+
     fun queryGroup(query: String) = query(query).first()
+
+    fun <T : Any> queryGroup(query: String, clazz: KClass<T>) = query(query, clazz).first()
+
+    fun <T : Any> queryGroup(query: String, clazz: Class<T>) = queryGroup(query, clazz.kotlin)
 
     fun query(query: String): List<Map<Key, List<K>>> =
             reader.parseQueryResult(
                     client.query(Query(query, database))
             )
+
+    fun <T : Any> query(query: String, clazz: KClass<T>): List<Map<Key, List<T>>> =
+            ClassMappingIntrospector.reader(clazz, registry).parseQueryResult(
+                    client.query(Query(query, database))
+            )
+
+    fun <T : Any> query(query: String, clazz: Class<T>) = query(query, clazz.kotlin)
 }
